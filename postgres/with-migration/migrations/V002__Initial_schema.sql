f3adc720bb3c665bb7e45f82c53614ea3dc014bb
KunalChoudhary521 <kunal.choudhary@mail.utoronto.ca>
2022-01-23 11:17:19 -0500

    POSTGRES: Add statefulset with migration via initcontainer

---
+++ postgres/with-migration/migrations/V002__Initial_schema.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE items (
+    productId     integer PRIMARY KEY,
+    name          varchar(20),
+    quantity      integer NOT NULL
+);
+
+INSERT INTO items VALUES (1, 'Snow globe', 8);
+INSERT INTO items VALUES (2, 'Sriracha sauce', 14);
+INSERT INTO items VALUES (3, 'Christmas tree', 2);
